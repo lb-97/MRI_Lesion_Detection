72a5eb82d7255c89eded88ec6a9b296ecaf0dc03
Aceticia <liuchris34@gmail.com>
2022-04-07 21:21:44 -0400

    Adding pretraining transformer code

--- pretrain_transformer.py
+++ pretrain_transformer.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from torch.utils.data import (DataLoader, Dataset)
 
 from transformer import MultiViewTransformer
+from training_utils import TrainingModel
 
 import os
 import os.path as osp
@@ -50,3 +51,54 @@ class PretrainTransformerDataset(Dataset):
     def __len__(self):
         return len(self.lst_data_dir)
 
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    from pytorch_lightning import Trainer
+
+    parser = ArgumentParser()
+    parser.add_argument("--batch_size_generate", type=int, default=20)
+    parser.add_argument("--n_worker_generate", type=int, default=128)
+    parser.add_argument("--batch_size_pretrain", type=int, default=128)
+    parser.add_argument("--n_worker_pretrain", type=int, default=20)
+    parser.add_argument("--cache_root",
+                        type=str,
+                        default="./pretrain_transformer_dataset/")
+    parser = TrainingModel.add_model_specific_args(parser)
+    args = parser.parse_args()
+
+    # CNN model
+    cnn_state_dict_path = "./checkpoint.pt"
+    # TODO: Add pretrained CNN model here
+    cnn_model = CNN().load_state_dict(cnn_state_dict_path)
+
+    # Load image dataset
+    image_dataset = ImageDataset()  # TODO: Add image dataset here
+    image_loader = DataLoader(image_dataset,
+                              batch_size=args.batch_size_generate,
+                              shuffle=True,
+                              num_workers=args.n_worker_generate)
+
+    # Instantiate pretraining dataset
+    dataset = PretrainTransformerDataset(cnn_model=cnn_model,
+                                         image_loader=image_loader,
+                                         cache_root=args.cache_root)
+    loader = DataLoader(dataset,
+                        batch_size=args.batch_size_pretrain,
+                        shuffle=True,
+                        num_workers=args.n_worker_pretrain)
+
+    # Instantiate training model
+    trainer_model = TrainingModel(
+        args=args,
+        model_args={"Transformer": {}},
+        models={"Transformer": MultiViewTransformer},
+        model_forward_args={"Transformer": {
+            "mask": True
+        }},
+        model_order=["Transformer"])
+
+    # Instantiate trainer
+    # TODO: Add monitoring callbacks here
+    trainer = Trainer(accelerator="auto", max_epochs=50)
+    trainer.fit(trainer_model, loader)
